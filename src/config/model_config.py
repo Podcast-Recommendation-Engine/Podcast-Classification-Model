0001218c24760a5ae433e09d1501c92dcc811545
Ibrahimghali <ibrahim.elghali@outlook.com>
2025-12-17 18:20:09 +0100

    Refactor code structure for improved readability and maintainability

--- src/config/model_config.py
+++ src/config/model_config.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ 
 ANNOTATED_DATA_PATH= "data/annotated/podcasts_annotated.csv"
 MLFLOW_TRACKING_URI = "http://mlflow:5000"  # Changed from localhost to mlflow
 EXPERIMENT_NAME= "podcast-classification-kid-friendly"
